ae0b5caccddb25d07cf78fd7f45d47734ded3653
alicebyers5 <alice.byers@gov.scot>
2023-08-10 16:57:56 +0100

    Add empty setup script

---
+++ code/00_setup.R
@@ -0,0 +1,21 @@
+#########################################################################
+# Name of file - 00_setup.R
+# Health and Wellbeing (HWB) Census 
+#
+# Type - Reproducible Analytical Pipeline (RAP)
+# Written/run on - RStudio Desktop
+# Version of R - 4.2.2
+#
+# Description - Sets up environment required for running HWB RAP. 
+# This is the only file which requires manual changes before the 
+# RAP process is run.
+#########################################################################
+
+
+### 1 - Load packages ----
+
+
+### 2 - Load functions (from functions folder) ----
+
+
+### END OF SCRIPT ###
